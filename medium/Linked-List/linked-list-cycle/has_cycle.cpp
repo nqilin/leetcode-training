1b1d4bd5d676d9462d719aafb8c355c8ea4e7c72
Chansopheavaty Houl <114042966+nqilin@users.noreply.github.com>
2026-03-21 09:36:33 +0700

    feat: add C++ solution for LeetCode 141 Linked List Cycle

---
+++ medium/Linked-List/linked-list-cycle/has_cycle.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) return false;
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while (slow != fast) {
+            if (!fast || !fast->next) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};
+
+// Test
+int main() {
+    cout << "Cycle test" << endl;
+    return 0;
+}
