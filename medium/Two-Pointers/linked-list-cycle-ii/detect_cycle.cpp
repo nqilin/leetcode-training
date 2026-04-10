cf468d61c8ef927787d8cfab02ea506d4a07c306
Chansopheavaty Houl <114042966+nqilin@users.noreply.github.com>
2026-04-10 08:13:26 +0700

    Create detect_cycle.cpp

---
+++ medium/Two-Pointers/linked-list-cycle-ii/detect_cycle.cpp
@@ -0,0 +1,33 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        /*
+         * Find cycle entrance using fast-slow pointers.
+         * Approach: Floyd's Tortoise and Hare
+         * Time Complexity: O(n)
+         * Space Complexity: O(1)
+         */
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
